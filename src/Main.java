2963da544c539b5bde2bf30eb5877dea15c3ffc3
Aida Kerimova <aida0505@mail.ru>
2022-12-26 22:15:54 +0300

    added read.md

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        SalesManager salesManager = new SalesManager(new int[] {45, 725, 90});
+
+        salesManager.max();
+
+        System.out.println(salesManager.max());
 
 
     }
