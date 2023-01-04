5a5f2c8569bd16c3f62c0ca328de3a9b22781895
Aida Kerimova <aida0505@mail.ru>
2023-01-04 21:57:29 +0300

    added notice

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        SalesManager salesManager = new SalesManager(new int[] {45, 725, 90});
+        SalesManager salesManager = new SalesManager(new int[]{45, 725, 90});
 
         salesManager.max();
 
